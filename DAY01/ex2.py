2259b2ed593358683164c0941a722331b0423e31
SolomonChidera <solochidera10@gmail.com>
2023-10-12 15:34:17 +0100

    Exercise 2 'working with comments'

--- DAY01/ex2.py
+++ DAY01/ex2.py
@@ -1,1 +1,9 @@
 # A comment, this is so you can read your program later
+#Anything after the # is a comment and ignored by python.
+
+print("I could have code like this") # and the comment after is ignored
+
+# you can also use comment to "disable" or comment out code:
+#print("This won't run.")
+
+print("This will run.")
