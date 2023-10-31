9b665e86345d567616a509b389eb2f815df20268
SolomonChidera <solochidera10@gmail.com>
2023-10-31 20:21:01 +0100

    If statement in python

---
+++ DAY-4/if.py
@@ -0,0 +1,10 @@
+x = int(input("Render some numbers "))
+if x < 0:
+    x = 0
+    print("Negative turned zero")
+elif x == 0:
+    print('Zero');
+elif x == 1:
+    print("single")
+else:
+    print("More")
