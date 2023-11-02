23450fa1e157605cd42d9ff0d631351c04acdeb2
SolomonChidera <solochidera10@gmail.com>
2023-11-02 18:40:16 +0100

    Simplified function in python Two ways of defining a function: Making a function perform a task(1) Making a function return a value(2)

--- DAY-4/function.py
+++ DAY-4/function.py
@@ -3,4 +3,10 @@ def greet(first_name, last_name):
     print("Welcome aboard")
 
 
+def get_greet(name):
+    return f"Hi {name}"
+
+
+get = get_greet("Moniaar")
+print(get)
 greet("Solomon", "Chidera")
