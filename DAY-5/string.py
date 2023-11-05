49880e7d2dbee60ad3a8fc8273c010046992261a
SolomonChidera <solochidera10@gmail.com>
2023-11-05 22:44:25 +0100

    You can manipulate string way better with python than C programming

--- DAY-5/string.py
+++ DAY-5/string.py
@@ -1,3 +1,13 @@
+# Some of my examples might sound stupid
+"""\
+But you just need to know that my examples
+Are based on what am thinking at that moment
+The examples am talking about are the filler words
+"""
+# So Gracias
 text = "Solomon"
 times = 3 * text
 print(times + 'Moniaar')
+print("--------------------")
+print('Will this work maybe it will work, maybe?'
+      'It will work brother no fear')
