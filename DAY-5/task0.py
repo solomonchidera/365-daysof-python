b98bef782ae69ca3cb73b7e7234af0b0eb6c2784
SolomonChidera <solochidera10@gmail.com>
2023-11-06 01:11:20 +0100

    Printing task

---
+++ DAY-5/task0.py
@@ -0,0 +1,7 @@
+# Task: Write a Python script that prints exactly
+# "Programming is like building a multilingual puzzle
+# followed by a new line.
+
+# Solution:
+
+print('"Programming is like building a multilingual puzzle')
