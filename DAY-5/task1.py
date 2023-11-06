e501a11a3fb451cab081cf1d54345fbb137d3235
SolomonChidera <solochidera10@gmail.com>
2023-11-06 01:19:07 +0100

    **Using f-string** To print numbeeeeeer

---
+++ DAY-5/task1.py
@@ -0,0 +1,9 @@
+# Task
+
+# Complete the given source code in order to print
+# the integer stored in the variable number
+# followed by Battery street
+
+# Solution:
+number = 98
+print(f"{number} Battery street")
