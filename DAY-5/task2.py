9b73a42d677b17ada2a00fab99d70eafbb3e5c00
SolomonChidera <solochidera10@gmail.com>
2023-11-06 01:25:06 +0100

    **Using f-string** To print Floating number

---
+++ DAY-5/task2.py
@@ -0,0 +1,10 @@
+# Task:
+
+# Complete the source code in order to print the
+# float stored in the variable
+# number with a precision of 2 digits.
+
+# Solution:
+
+number = 3.14159
+print(f"Float: {number:.2f}")
