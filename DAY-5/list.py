52318df275a128200234f94d2cd2b95b87f68265
SolomonChidera <solochidera10@gmail.com>
2023-11-07 15:10:09 +0100

    Just a basic example of list in python

--- DAY-5/list.py
+++ DAY-5/list.py
@@ -1,6 +1,7 @@
 import random
 import math
 
-randList = ["string", 23.9, 20]
+randList = ["string", "bread", "Laptop", 23.9, 4, 7, 20, "Foo", "Bar"]
 sList = list(range(10))
-print(sList)
+
+print("Index of bread", randList.index("bread"))
