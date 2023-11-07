a9818317a8aa6e82f478cb0332e40b03956eb18b
SolomonChidera <solochidera10@gmail.com>
2023-11-07 15:21:21 +0100

    Using the capitalize method in python

---
+++ DAY-6/capitalize.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    def userName(first, last):
+        first = first.capitalize()
+        last = last.capitalize()
+        return first + " " + last
+
+
+    print(userName("solomon", "chidera"))
