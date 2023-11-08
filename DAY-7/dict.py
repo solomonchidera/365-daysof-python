442219ef40f88800ab0197819cc83eeda2c1b207
SolomonChidera <solochidera10@gmail.com>
2023-11-08 22:55:09 +0100

    Dictionary in python

--- DAY-7/dict.py
+++ DAY-7/dict.py
@@ -5,6 +5,10 @@ if 'Age' in dict:
     print("Name exist in dictionary")
 keys = dict.keys()
 value = dict.values()
+dict['Country'] = 'USA'
 
 for key in dict:
     print(key, dict[key])
+
+for key, value in dict.items():
+    print(key, value)
