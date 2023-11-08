c901f912bdecc3bb48e8d274fa818ccfd0ac623b
SolomonChidera <solochidera10@gmail.com>
2023-11-08 09:01:47 +0100

    Append method used in this list

--- DAY-7/append.py
+++ DAY-7/append.py
@@ -1,6 +1,7 @@
 if __name__ == '__main__':
     def main():
         colors = ['Green', 'Yellow', 'Orange', 'White']
+        colors.append('Black')
         for color in colors:
             print(color)
     main()
